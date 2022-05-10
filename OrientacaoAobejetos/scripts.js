c69f315b3b5ee679cc997c0be1b9e4cb7960962f
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2022-05-10 12:21:44 -0300

    criação do arquivo scrips

---
+++ OrientacaoAobejetos/scripts.js
@@ -0,0 +1,8 @@
+class ContaBancaria {
+    constructor(agencia, numero, tipo, saldo) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this.saldo = saldo;
+    }
+}
