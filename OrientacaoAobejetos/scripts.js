ddb85b83562d0f25312211b8f2b0499f86b40a3c
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2022-05-10 12:27:26 -0300

    incrementação do script

--- OrientacaoAobejetos/scripts.js
+++ OrientacaoAobejetos/scripts.js
@@ -3,6 +3,29 @@ class ContaBancaria {
         this.agencia = agencia;
         this.numero = numero;
         this.tipo = tipo;
-        this.saldo = saldo;
+        this._saldo = saldo;
+    }
+    
+    get saldo() {
+        return this._saldo;
+    }
+
+    set saldo(valor) {
+        this.saldo = valor;
+    }
+
+    sacar(valor) {
+        if (valor > this._saldo) {
+            return "Operação negada"
+        }
+        this._saldo = this._saldo - valor;
+
+        return this._saldo
+    }
+
+    depositar(valor) {
+        this._saldo = this._saldo + valor;
+
+        return this._saldo;
     }
 }
