20c7565520c73f428eb94142addcbfe01a7a9dcf
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2022-05-11 12:00:06 -0300

    finalização do ex001

--- Map-Filter-Reduce/ex001.js
+++ Map-Filter-Reduce/ex001.js
@@ -1,5 +1,20 @@
+const maca = {
+    value: 2,
+}
+
+const laranja  = {
+    value: 3,
+}
+
+
 function mapComThis(arr, thisArg) {
     return arr.map(function(item){
-        item * this.value;
+     return item * this.value;
     }, thisArg);
 }
+
+const nums = [1, 2];
+
+console.log('this -> maçã', mapComThis(nums, maca));
+
+console.log('this -> laranja', mapComThis(nums, laranja));
