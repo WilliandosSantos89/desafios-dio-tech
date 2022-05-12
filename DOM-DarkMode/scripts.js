51ae1fb9d8d3476dbb41d19a6ad4ddae419eb88f
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2022-05-12 11:47:07 -0300

    Update scripts.js

--- DOM-DarkMode/scripts.js
+++ DOM-DarkMode/scripts.js
@@ -1,9 +1,30 @@
 function changeMode() {
-    console.log('cliquei!');
+    changeClasses();
 }
 
+function changeClasses() {
+    button.classList.toggle('darkModeClass');
+    h1.classList.toggle('darkModeClass');
+    body.classList.toggle('darkModeClass');
+    footer.classList.toggle('darkModeClass');
+}
+
+function changeText() {
+
+    const lightMode = "Light Mode";
+    const darkMode = "Dark Mode";
+
+    if(body.classList.contains('darkModeClass')) {
+        body.innerHTML = lightMode;
+        h1.innerHTML = darkMode + " ON"
+    }
+}
+
+const darkModeClass = 'dark-mode';
 const button = document.getElementById('mode-selector');
 const h1 = document.getElementById('page-title');
-const body = document.get
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
 
 button.addEventListener('click', changeMode);
