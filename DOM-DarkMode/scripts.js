758517fc6694b5b6d35a4e00cd7c49a1da2ff0a7
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2022-05-12 11:23:42 -0300

    construção do js

---
+++ DOM-DarkMode/scripts.js
@@ -0,0 +1,9 @@
+function changeMode() {
+    console.log('cliquei!');
+}
+
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.get
+
+button.addEventListener('click', changeMode);
